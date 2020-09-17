9c260b93879a6c41092698c80898bc06bf02ee02
GuðmundurA <gudmundura20@ru.is>
2020-09-17 12:09:07 +0000

    defs

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,43 +1,49 @@
 # Við ætlum fyrst að gera 4 föll, 1 fyrir hverja átt
-# Gerum fall sem gefur upp hvaða áttir eru í boði 
+# Gerum fall sem gefur upp hvaða áttir eru í boði  #DONE
 # Gerum svo annað fall fyrir meldingu þegar farið er í átt sem er ekki leyfð
 # Gerum fall fyrir hreyfingu á milli gólfflísa (for loop)
 # Gerum fall sem að gefur meldingu um að leikurinn hafi klárast (victory)
 # Föllin eiga við hnitakerfi (x,y) svo þau taka x og y arguments
-# Get ég núna pushað
 # grid = 3x, 3y
+# lentum í github veseni í klukkutíma
 import math
-def get_dir():
-        (x):
-            x = int(input("Give direction West or East: "))
-    y = int(input("Give direction North or South: "))
-    return x, y
-
-def get_grid():
-    x1, y1 = 1, 1
-    x2, y2 = 2, 2
-    x3, y3 = 3, 3
-    return x, y
-    
-    
-def mv_n():
-    '''Fara norður um einn frá pos í framköllunar scope''' 
-    x += 1
-    print(directions) # frá nýja tile í pos x+1, y
-    
-def mv_s():
-    '''Fara suður um einn frá pos í framköllunar scope'''
+def move_(y):
+    y += 1
     y -= 1
-    print(directions) # x, y-1
+    return (beiðni)
+def
+    
 
+    
+def get_dir(n, s, a, v):
+    ''' skilaboð til notanda um leifðar áttir'''
+        beiðni = "Þú getur farið til: "
+        if pos_x == 1 and pos_y == 1:
+            beiðni += "(N)orth"
+        if pos_x == 1 and pos_y == 2:
+            beiðni += "(N)orth" or "(E)ast" or "(W)est"
+        if pos_x == 1 and pos_y == 3:
+            beiðni += "(E)ast" or "(W)est"
+        if pos_x == 2 and pos_y == 3:
+            beiðni += "(E)ast" or "(W)est"
+        if pos_x == 3 and pos_y == 3:
+            beiðni += "(W)est" or "(S)outh"
+        if pos_x == 3 and pos_y == 2:
+            beiðni += "(N)orth" or "(S)outh"
+        if pos_x == 3 and pos_y == 1:
+            beiðni += "Victory!"
+        if pos_x == 2 and pos_y == 2:
+            beiðni += "(W)est" or "(S)outh"
+        if pos_x == 2 and pos_y == 1:
+            beiðni += "(N)orth"
+    print (beiðni + ".")
+        
 
-starting_pos_x = x1
-starting_pos_y = y1
-get_dir(x)
-get_dir(y)
+n = y +1
+s = y -1
+e = x +1
+v = x -1
 
-# if pos_xy = x1, y1:
-   # print("You can travel N(orth).")
 
 # íhuga func sem tekur input, biðja um dir og túlka það
 # skiptir máli hvar þú ert staddur, hvaða áttir eru gildar
