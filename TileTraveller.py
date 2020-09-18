58a7cd78cb2e75d809fcf303e4767b146bc29088
GuðmundurA <gudmundura20@ru.is>
2020-09-18 01:48:37 +0000

    Input function

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,14 +1,14 @@
 # Við ætlum fyrst að gera upplýsingar til notanda hvert hann getur farið:
-# Gerum fall sem gefur upp hvaða áttir eru í boði 
-# Gerum svo annað fall fyrir meldingu þegar farið er í átt sem er ekki leyfð
-# Notandi þarf að reyna að velja réttar leiðir til að komast á sigurflísina
+#   Gerum fall sem gefur upp hvaða áttir eru í boði 
+#   Gerum svo annað fall fyrir meldingu þegar farið er í átt sem er ekki leyfð
+#   Notandi þarf að reyna að velja réttar leiðir til að komast á sigurflísina
 
-# Gerum fall fyrir hreyfingu á milli gólfflísa (for loop)
+# Gerum fall fyrir hreyfingu á milli gólfflísa: 
 # Föllin eiga við hnitakerfi (x,y) svo þau taka x og y arguments
 # grid = 3x, 3y
 
 # íhuga func sem tekur input, biðja um dir og túlka það
-# skiptir máli hvar þú ert staddur, hvaða áttir eru gildar
+# func sem túlkar gildar áttir frá staðsetningu
 
 # lentum í github veseni í klukkutíma sagðir okkur að setja þetta komment
 # https://github.com/Gudmundura20/Timaverkefni8
@@ -17,8 +17,8 @@
 def game_end():
     '''exit program with built in func exit()'''
     exit()
-
-def get_dir(n, s, a, v):   # Ef sett er inn n, s, a, v
+# fastar a, v = austur, vestur
+def get_directions(n, s, a, v):   # Ef sett er inn n, s, a, v
     '''skilaboð til notanda um leyfðar áttir'''
     beiðni= "You can travel: "
     if x == 1 and y == 1:
@@ -42,24 +42,30 @@ def get_dir(n, s, a, v):   # Ef sett er inn n, s, a, v
         game_end() # exits program
     print (beiðni + ".")  # breytan efst + leyfðar áttir á ensku skv mimir output
     
-# notandi gefur input fyrir áttirnar
-def move_direction_input(n, s, a, v):
-    n = input("n ")
-    s = input("s ")
-    a = input("e ")
-    v = input("v ")
-    return direction_func(n, s, a, v)
+def direction_direct(n, s, a, v):
+    '''Notandi stýrir áttum'''
+    direction = input("Direction: ")
+    if input == 'n':
+        'n' == 'N'
+    if input == 's':
+        's' == 'S'
+    if input == 's':
+        'a' == 'A'
+    if input == 's':
+        'v' == 'V'
+    return direction
     
-def direction_func(n, s, a, v):
-    '''Hreyfing eftir input færir position x,y í nýjann ramma'''
-    n = y +1
+def direction_direct_func(direction, x, y):
+    '''Hreyfing eftir input færir position x,y í nýja flís'''
+    for direction in direction_direct(n, s, a, v):
+        n = y +1
     
-    s = y -1
+        s = y -1
 
-    a = x +1
+        a = x +1
 
-    v = x -1
-    return x, y
+        v = x -1
+        return x, y
       
 x = 1
 y = 1
@@ -69,9 +75,9 @@ a = 1
 v = 1
 
 while x != 3 and y != 1:
-    get_dir(n, s, a, v) # Hvaða áttir eru í boði
-    move_direction_input(n, s, a, v) # notandi velur átt
-    direction_func(n, s, a, v) # notandi færist
+    get_directions(n, s, a, v) # Hvaða áttir eru í boði
+    direction_direct(n, s, a, v) # notandi velur átt
+    direction_direct_func(direction, x, y) # notandi færist
 
 
 
